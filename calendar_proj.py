d1b9402f7004ccd48e7a46f4b7444c763b42335e
Shai Benjamin <sheyeh@gmail.com>
2015-10-24 22:25:16 +0300

    add next/prev month

--- calendar_proj.py
+++ calendar_proj.py
@@ -7,8 +7,11 @@ PORT = int(os.environ.get("PORT", 5000))
 
 CENTER_START = '<center><font face="verdana">'
 CENTER_END = "</center></font>"
-PREV_NEXT = SimpleTemplate(
+PREV_NEXT_YEAR = SimpleTemplate(
     '<font size="5"><a href="{{prev}}">{{prev}}</a> {{year}} <a href="{{next}}">{{next}}</a></font>'
+)
+PREV_NEXT_MONTH = SimpleTemplate(
+    '<font size="5"><a href="/{{prev_year}}/{{prev_month}}">{{prev_name}}</a> {{month}} <a href="/{{next_year}}/{{next_month}}">{{next_name}}</a></font>'
 )
 LINK_MONTH = SimpleTemplate(
     '<a href="{{year}}/{{month_num}}">{{month_name}}</a>'
@@ -40,7 +43,7 @@ def cal_year(year=None):
     year = max((year, 2))
     yearly_cal = CENTER_START
     yearly_cal += calendar.HTMLCalendar(6).formatyear(theyear=year)
-    bottom = PREV_NEXT.render(next=year+1, year=year, prev=year-1)
+    bottom = PREV_NEXT_YEAR.render(next=year+1, year=year, prev=year-1)
     yearly_cal +=  "<br>" + bottom
     yearly_cal += CENTER_END
     yearly_cal = year_cal_with_links(yearly_cal, year)
@@ -50,6 +53,20 @@ def cal_year(year=None):
 def cal_month(year, month):
     monthly_cal = CENTER_START
     monthly_cal += calendar.HTMLCalendar(6).formatmonth(themonth=month, theyear=year)
+    now = datetime.date(year=year, month=month, day=15)
+    one_month = datetime.timedelta(30)
+    prev_month = now - one_month
+    next_month = now + one_month
+    bottom = PREV_NEXT_MONTH.render(
+        prev_year=prev_month.year,
+        prev_month=prev_month.month,
+        prev_name=calendar.month_name[prev_month.month],
+        month=calendar.month_name[month],
+        next_year=next_month.year,
+        next_month=next_month.month,
+        next_name=calendar.month_name[next_month.month]
+    )        
+    monthly_cal += "<br>" + bottom
     monthly_cal += CENTER_END
     monthly_cal = month_cal_with_links(monthly_cal, year)
     return repad_respace(monthly_cal)
